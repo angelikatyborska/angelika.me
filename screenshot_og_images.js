d621b609455597096d091e0e59ae7ef41596de37
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-12-25 15:52:49 +0100

    Create default og image

--- screenshot_og_images.js
+++ screenshot_og_images.js
@@ -6,6 +6,9 @@ const fs = require('fs');
   const page = await browser.newPage();
   await page.setViewport({width: 1200, height: 630});
 
+  await page.goto(`http://0.0.0.0:3333/default_og_image/`);
+  await page.screenshot({ path: `_assets/images/og_images/default.png`})
+
   const slugs = fs.readdirSync('./_site/og_images')
 
   for ( let i = 0; i < slugs.length; i++){
