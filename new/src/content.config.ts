f74debb63ba49b52947bf6e6c762fcd447b00cde
Angelika Cathor <angelikacathor@fastmail.com>
2024-12-09 20:39:03 +0100

    Reproduce blog post URLs

--- new/src/content.config.ts
+++ new/src/content.config.ts
@@ -7,11 +7,11 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
+		excerpt: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		tags: z.array(z.string())
 	}),
 });
 
---
+++ new/src/lib/posts.ts
@@ -0,0 +1,15 @@
+import {getCollection} from "astro:content";
+
+export const getPosts = async () => {
+  const posts = await getCollection('blog');
+
+  // TODO: unit tests
+  const reformatDateInId = (postId: string) => {
+    return postId.replace(/^(\d{4})-(\d{2})-(\d{2})-(.*)/, (_, year, month, day, title) => `${year}/${month}/${day}/${title}`)
+  }
+
+  return posts.map((post) => ({
+    ...post,
+    id: reformatDateInId(post.id),
+  }));
+}
