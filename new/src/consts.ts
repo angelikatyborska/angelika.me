909ce4fc75eaa1cff7b73835e2971db6991c5dea
Angelika Cathor <angelikacathor@fastmail.com>
2024-12-27 19:03:54 +0100

    Recreate social icons

--- new/src/consts.ts
+++ new/src/consts.ts
@@ -4,3 +4,12 @@
 export const SITE_TITLE = "Angelika.me";
 export const SITE_DESCRIPTION =
   "Angelika Cathor's personal blog about web development (HTML, CSS, JavaScript, Elixir, accessibility).";
+
+export const REPO = "https://github.com/angelikatyborska/angelika.me";
+export const LINKEDIN = "https://linkedin.com/in/angelika-tyborska-330a6bb0";
+export const MASTODON = "https://mas.to/@angelikatyborska";
+export const BLUESKY = "https://bsky.app/profile/angelikatyborska.bsky.social";
+export const GITHUB = "https://github.com/angelikatyborska";
+export const EXERCISM = "https://exercism.io/profiles/angelikatyborska";
+export const LASTFM = "https://www.last.fm/user/atyborska93";
+export const FLICKR = "https://www.flickr.com/photos/192384616@N03";
