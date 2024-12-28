f50f096b0d00802a85aacf915b864650fd6c1431
Angelika Cathor <angelikacathor@fastmail.com>
2024-12-28 20:08:59 +0100

    Migrate header

--- new/src/lib/consts.ts
+++ new/src/lib/consts.ts
@@ -3,7 +3,7 @@ export const SITE_DESCRIPTION =
   "Angelika Cathor's personal blog about web development (HTML, CSS, JavaScript, Elixir, accessibility).";
 
 export const REPO = "https://github.com/angelikatyborska/angelika.me";
-export const LINKEDIN = "https://linkedin.com/in/angelika-tyborska-330a6bb0";
+export const LINKEDIN = "https://linkedin.com/in/angelika-cathor";
 export const MASTODON = "https://mas.to/@angelikatyborska";
 export const BLUESKY = "https://bsky.app/profile/angelikatyborska.bsky.social";
 export const GITHUB = "https://github.com/angelikatyborska";
