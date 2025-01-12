78223ca2453d1992d37f9f45f3fddd8da967f03c
Angelika Cathor <angelikacathor@fastmail.com>
2025-01-12 15:15:30 +0100

    Fix verify_og_images

--- bin/verify_og_images.cjs
+++ bin/verify_og_images.cjs
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 const slugs = fs.readdirSync("src/content/blog/");
-const paths = [`public/og/default.png`, ...slugs.map((slug) => `public/og/${slug}.png`)];
+const paths = [`public/og/default.png`, ...slugs.filter(slug => !slug.startsWith('.')).map((slug) => `public/og/${slug}.png`)];
 
 const missingImages = paths.filter((path) => {
-  return !fs.statSync(path, { throwIfNoEntry: false }) && !path.startsWith(".");
+  return !fs.statSync(path, { throwIfNoEntry: false });
 });
 
 if (missingImages.length > 0) {
