6a570218e5c28589db967df95dea2869a446ea6b
Angelika Cathor <angelikacathor@fastmail.com>
2025-01-12 15:19:56 +0100

    Format

--- bin/verify_og_images.cjs
+++ bin/verify_og_images.cjs
@@ -1,7 +1,10 @@
 const fs = require("fs");
 
 const slugs = fs.readdirSync("src/content/blog/");
-const paths = [`public/og/default.png`, ...slugs.filter(slug => !slug.startsWith('.')).map((slug) => `public/og/${slug}.png`)];
+const paths = [
+  `public/og/default.png`,
+  ...slugs.filter((slug) => !slug.startsWith(".")).map((slug) => `public/og/${slug}.png`),
+];
 
 const missingImages = paths.filter((path) => {
   return !fs.statSync(path, { throwIfNoEntry: false });
